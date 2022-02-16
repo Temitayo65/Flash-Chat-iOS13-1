f98ab12dd9d35ad6e2ca7d482a714238ac9d9156
temitayo65 <iloritemitayo65@gmail.com>
2022-02-16 21:30:56 +0100

    Add retrieval functionality to chatViewController

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -10,31 +10,104 @@ import UIKit
 import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    let db = Firestore.firestore()
+    
+    var messages : [Message] = [
+        Message(sender: "1@2.com", body: "Hey"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         title = K.appName
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
+    }
+    
+    func loadMessages() {
+        messages = []
+        db.collection(K.FStore.collectionName).getDocuments() { (querySnapshot, error) in
+            if let e = error {
+                print("There was an issue in retrieving data From Firestore: \(e)")
+            } else {
+                if let snapShotDocuments = querySnapshot?.documents{
+                    for doc in snapShotDocuments{
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField : messageSender, K.FStore.bodyField : messageBody]) { error in
+                if let e = error{
+                    print("There was an issue saving data to firestore, \(e)")
+                }
+                else{
+                    print("Successfully saved data")
+                }
+            }
+            
+        }
+        
+        
     }
     
     @IBAction func logoutPressed(_ sender: Any) {
-    
-    do {
-      try Auth.auth().signOut()
-        navigationController?.popToRootViewController(animated: true)
         
-    } catch let signOutError as NSError {
-      print("Error signing out: %@", signOutError)
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+        
+        
     }
-      
-        
+    
+}
+
+
+extension ChatViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+            as? MessageCell{
+            cell.label.text =  messages[indexPath.row].body
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+}
+
+extension ChatViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
     
 }
--- Flash Chat iOS13/Models/Message.swift
+++ Flash Chat iOS13/Models/Message.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-import Foundation
+struct Message{
+    let sender: String
+    let body: String
+    
+    
+}
