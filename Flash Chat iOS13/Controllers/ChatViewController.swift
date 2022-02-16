4c9c12525df87be86e090bd1777a648f772e3b07
temitayo65 <iloritemitayo65@gmail.com>
2022-02-17 00:28:20 +0100

    Enable mulit-user interaction

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -45,6 +45,8 @@ class ChatViewController: UIViewController {
                             self.messages.append(newMessage)
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
                             }
                             
                         }
@@ -68,7 +70,10 @@ class ChatViewController: UIViewController {
                 }
                 else{
                     print("Successfully saved data")
-                    self.messageTextfield.text = ""
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
+                    
                 }
             }
             
@@ -102,7 +107,23 @@ extension ChatViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
             as? MessageCell{
-            cell.label.text =  messages[indexPath.row].body
+            let message = messages[indexPath.row]
+            cell.label.text =  message.body
+            
+            if message.sender == Auth.auth().currentUser?.email{
+                cell.leftImageView.isHidden = true
+                cell.rightImageView.isHidden = false
+                cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+                cell.label.textColor = UIColor(named: K.BrandColors.purple)
+            }
+            else{
+                cell.leftImageView.isHidden = false
+                cell.rightImageView.isHidden = true
+                cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+                cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+            }
+            
+           
             return cell
         }
         return UITableViewCell()
